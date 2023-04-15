91da3dd287b1f8a836dd2792732be4e876d41530
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-04-15 09:02:57 -0700

    Adds a task to the list.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners();
 });
+
+function addingEventListeners() {
+  document
+    .querySelector("#create-task-form")
+    .addEventListener("submit", handleSubmit);
+}
+
+function handleSubmit(e){
+  e.preventDefault();
+  const newTask = e.target["new-task-description"].value;
+  
+  displayTask(newTask);
+}
+
+function displayTask(newTask){
+  const tasksUL = document.querySelector("#tasks");
+  const taskLi = document.createElement("li");
+  
+  taskLi.textContent = newTask;
+  tasksUL.appendChild(taskLi);
+}
